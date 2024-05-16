197500e47cf33da752ff354485aea240badfd232
yusuftalhabek <101089459+yusuftalhabek@users.noreply.github.com>
2024-05-16 21:40:06 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,22 +6,28 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
-
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Load the FXML file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Sample.fxml"));
+            AnchorPane root = loader.load();
+            
+            // Set up the scene
+            Scene scene = new Scene(root);
+
+            // Set the scene to the stage and show it
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Audio Visualization");
+            primaryStage.show();
+        } catch(Exception e) {
+            // Print stack trace for any exceptions
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- SampleController.java
+++ SampleController.java
@@ -1,47 +1,105 @@
 package application;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.chart.Axis;
 import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SampleController {
-	@FXML
-	private Button SelectFile;
-	@FXML
-	private Button StartRecording;
-	private int[] audioData;
-	@FXML
-	private LineChart<Number,Number> Graphic;
-	@FXML
-	private void ActionStartRecording(ActionEvent e)
-	{
-		
-	}
-	@FXML
-	private void ActionSelectFile(ActionEvent e)
-	{
-		String audioFilePath;
-		audioFilePath = "/Users/yusuftalhabek/Desktop/HeartbeatWorkspace/Heart/dong3_Heart.wav";
-		audioData = Amplitude.extractAmplitudeData(audioFilePath);
-		
-		NumberAxis xAxis = new NumberAxis();
-        NumberAxis yAxis = new NumberAxis();
+    @FXML
+    private Button SelectFile;
+    @FXML
+    private Button StartRecording;
+    @FXML
+    private LineChart<Number, Number> Graphic;
 
-        LineChart<Number, Number> Graphic = new LineChart<>(xAxis, yAxis);
-        
-        XYChart.Series<Number, Number> series = new XYChart.Series<>();
-        
+    private TargetDataLine microphone;
+    private Thread recordingThread;
 
-        double xScale = 1.0; // Scaling factor for x-axis
-        for (int i = 0; i < audioData.length; i++) {
-            series.getData().add(new XYChart.Data<>(i * xScale, audioData[i]));
+    @FXML
+    private void ActionStartRecording(ActionEvent e) {
+        startMicrophoneRecording();
+    }
+
+    @FXML
+    private void ActionSelectFile(ActionEvent e) {
+        File audioFile = new File("path/to/your/audio/file.wav");
+        processAudioFile(audioFile);
+    }
+
+    private void startMicrophoneRecording() {
+        AudioFormat format = new AudioFormat(44100, 16, 1, true, true);
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+        if (!AudioSystem.isLineSupported(info)) {
+            System.out.println("Microphone not supported");
+            return;
         }
+        try {
+            microphone = (TargetDataLine) AudioSystem.getLine(info);
+            microphone.open(format);
+            microphone.start();
+            recordingThread = new Thread(() -> {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while (true) {
+                    bytesRead = microphone.read(buffer, 0, buffer.length);
+                    if (bytesRead > 0) {
+                        List<Integer> amplitudes = extractAmplitudes(buffer, bytesRead);
+                        updateChart(amplitudes);
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                }
+            });
+            recordingThread.start();
+        } catch (LineUnavailableException ex) {
+            ex.printStackTrace();
+        }
+    }
 
-        Graphic.getData().add(series);
-        
-	}
+    private List<Integer> extractAmplitudes(byte[] buffer, int bytesRead) {
+        List<Integer> amplitudes = new ArrayList<>();
+        for (int i = 0; i < bytesRead; i += 2) {
+            int amplitude = (buffer[i] << 8) | (buffer[i + 1] & 0xFF);
+            amplitudes.add(amplitude);
+        }
+        return amplitudes;
+    }
+
+    private void updateChart(List<Integer> amplitudes) {
+        Platform.runLater(() -> {
+            XYChart.Series<Number, Number> series = new XYChart.Series<>();
+            for (int i = 0; i < amplitudes.size(); i++) {
+                series.getData().add(new XYChart.Data<>(i, amplitudes.get(i)));
+            }
+            Graphic.getData().clear();
+            Graphic.getData().add(series);
+        });
+    }
+
+    private void processAudioFile(File audioFile) {
+        try (AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audioFile)) {
+            AudioFormat format = audioInputStream.getFormat();
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = audioInputStream.read(buffer)) != -1) {
+                List<Integer> amplitudes = extractAmplitudes(buffer, bytesRead);
+                updateChart(amplitudes);
+                Thread.sleep(1000);
+            }
+        } catch (UnsupportedAudioFileException | IOException | InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
